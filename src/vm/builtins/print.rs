32f09e2347b416ff3273022031f4243822ac56b2
Dorje <dorjegilfillan@gmail.com>
2023-04-04 18:24:14 +0200

    added newline to end of print function

--- src/vm/builtins/print.rs
+++ src/vm/builtins/print.rs
@@ -5,5 +5,6 @@ pub fn print(vm: &mut Vm, this: PettyObject, args: FuncArgs) -> PettyObject {
     for arg in args.0 {
         print!("{} ", arg.force_repr(vm));
     }
+    println!();
     PtyNull.into()
 }
